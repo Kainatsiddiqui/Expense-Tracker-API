bb09b7dfb8fb51e353d13e5aa3af878aef1c3043
Kainat Siddiqui <kainat.siddique08@gmail.com>
2026-08-05 21:11:09 +0530

    Import Vercel frontend URL

--- expense-tracker-frontend/src/api/client.ts
+++ expense-tracker-frontend/src/api/client.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 /* Creating the api client */
 
+
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 api.interceptors.request.use((config) => {
